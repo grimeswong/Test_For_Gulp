096044a5871d18138d297e392d5b2ae1b4293a90
Grimes <grimeswong@gmail.com>
2016-10-30 12:31:19 +1000

    add minify-css, autopreix and concat tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');	//Requires the gulp-sass plugin
 var imagemin = require('gulp-imagemin'); //Requires the gulp-imagemin plugin // npm install gulp-imagemin --save-dev
 var changed = require('gulp-changed'); // don't process the unchanged files
+var cleanCSS = require('gulp-clean-css'); // Requires the gulp-clean-css plugin // npm install gulp-clean-css --save-dev
+var autoprefix = require('gulp-autoprefixer'); // Requires the gulp-autoprefixer plugin // npm install gulp-autoprefixer --save-dev
+var concat = require('gulp-concat'); //Requires the gulp-concat plugin // npm install gulp-autoprefixer --save-dev
 
 
 /***** Sample only *****
@@ -13,28 +16,37 @@ gulp.task('task-name', function() {
 /***** Sample only *****/
 
 
-
-gulp.task ('hello', function () {	// print out "Hello World"
+/***** print out "Hello World" *****/
+gulp.task ('hello', function () {
 	console.log('Hello Grimes, well done, you make it!!!');
 });
 
-gulp.task('sass', function () { //Compile sass to css
+/***** Compile sass to css *****/
+gulp.task('sass', function () {
 	return gulp.src('work/src/styles/**/*') // source in root folder and child folders
 	.pipe(sass()) //Using gulp-sass
 	.pipe(gulp.dest('work/build/styles'))
-
 });
 
+/***** Watch the task change, if any chnage in the folder, the "sass" will be process and outputs css files to the destination folder *****/
 gulp.task('watch', function() {
-	
+
 	// gulp.watch('files-to-watch', ['tasks','to','run']); // gulp watch syntax
-	gulp.watch('work/src/styles/**/*', ['sass']); //gulp watch 
+	gulp.watch('work/src/styles/**/*', ['sass']); //gulp watch
 });
 
-gulp.task('imagemin', function() {	// Compress the image file and outputs to the dest folder
+/***** Compress the image file and outputs to the dest folder ******/
+gulp.task('imagemin', function() {
 	var img_src ='work/src/images/**/*', img_dest = 'work/build/images';
 	gulp.src(img_src)
 	.pipe(changed(img_dest))
 	.pipe(imagemin())
 	.pipe(gulp.dest(img_dest));
 });
+
+/***** Minify the css file *****/
+gulp.task ('styles', function(){
+  gulp.src('work/src/styles/minify.css')
+  .pipe(concat('styles.css')
+  .pipe(gulp.dest('work/build/styles'));
+})
